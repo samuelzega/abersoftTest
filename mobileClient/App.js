b60af1dd83a2d39e00047a99748aae0c2cfc1b22
samuelzega <samuel.zegaa@gmail.com>
2020-06-17 08:46:21 +0700

    update login and register screen

--- mobileClient/App.js
+++ mobileClient/App.js
@@ -1,83 +1,43 @@
 import React from 'react'
-import {
-   StyleSheet,
-   Text,
-   View,
-   Dimensions,
-   StatusBar,
-   TouchableOpacity
-} from 'react-native'
-import { LinearGradient } from 'expo-linear-gradient'
-import { Modalize } from 'react-native-modalize'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { Home, Login, Register, Welcome } from './src/screens'
+const Stack = createStackNavigator()
 export default function App() {
    return (
-      <View
-         style={{
-            flex: 1,
-            alignItems: 'center',
-            backgroundColor: '#4ED2DA'
-         }}
-      >
-         <StatusBar barStyle={'light-content'} backgroundColor="#3549FB" />
-         <LinearGradient
-            colors={['#3549FB', '#4ED2DA']}
-            style={{
-               position: 'absolute',
-               left: 0,
-               right: 0,
-               top: 0,
-               height: Dimensions.get('screen').height * 0.77
-            }}
-         />
-         <View
-            style={{
-               marginTop: Dimensions.get('screen').height * 0.5,
-               flex: 1,
-               alignContent: 'center',
-               justifyContent: 'center'
-            }}
-         >
-            <TouchableOpacity
-               style={{
-                  backgroundColor: '#fff',
-                  width: Dimensions.get('screen').width * 0.7,
-                  paddingVertical: 5,
-                  alignItems: 'center',
-                  borderRadius: 100,
-                  marginVertical: Dimensions.get('screen').height * 0.027
-               }}
-            >
-               <Text style={{ color: '#000', fontFamily: 'Roboto' }}>
-                  Register Account
-               </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-               style={{
-                  backgroundColor: '#fff',
-                  width: Dimensions.get('screen').width * 0.7,
-                  paddingVertical: 5,
-                  alignItems: 'center',
-                  borderRadius: 100,
-                  marginVertical: Dimensions.get('screen').height * 0.027
-               }}
-            >
-               <Text style={{ color: '#000', fontFamily: 'Roboto' }}>
-                  Login
-               </Text>
-            </TouchableOpacity>
-         </View>
-      </View>
+      <NavigationContainer>
+         <Stack.Navigator>
+            <Stack.Screen
+               name="Welcome"
+               component={Welcome}
+               options={{ headerShown: false }}
+            />
+            <Stack.Screen
+               name="Login"
+               component={Login}
+               options={{ headerShown: false }}
+            />
+            <Stack.Screen
+               name="Register"
+               component={Register}
+               options={{ headerShown: false }}
+            />
+            <Stack.Screen
+               name="Home"
+               component={Home}
+               options={{ headerShown: false }}
+            />
+         </Stack.Navigator>
+      </NavigationContainer>
    )
 }
 
 const styles = StyleSheet.create({
    container: {
       flex: 1,
+      backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'orange'
-   },
-   gradient: {
-      flex: 1
+      justifyContent: 'center'
    }
 })
---
+++ mobileClient/src/screens/Login/index.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import {
+   StyleSheet,
+   Text,
+   View,
+   Dimensions,
+   StatusBar,
+   TouchableOpacity,
+   TextInput,
+   KeyboardAvoidingView
+} from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import { Ionicons } from '@expo/vector-icons'
+import { ScrollView } from 'react-native-gesture-handler'
+
+export default function Login({ navigation }) {
+   return (
+      <View
+         style={{
+            flex: 1,
+            alignItems: 'center',
+            backgroundColor: '#4ED2DA'
+         }}
+      >
+         <StatusBar barStyle={'light-content'} backgroundColor="#3549FB" />
+         <LinearGradient
+            colors={['#3549FB', '#4ED2DA']}
+            style={{
+               position: 'absolute',
+               left: 0,
+               right: 0,
+               top: 0,
+               height: Dimensions.get('screen').height * 0.77
+            }}
+         />
+         <TouchableOpacity
+            style={{
+               alignSelf: 'flex-start',
+               paddingStart: 20,
+               paddingTop: 10,
+               height: Dimensions.get('screen').height * 0.1
+            }}
+            onPress={() => navigation.navigate('Welcome')}
+         >
+            <Ionicons name="md-arrow-back" size={24} color="white" />
+         </TouchableOpacity>
+         <ScrollView
+            style={{
+               width: Dimensions.get('screen').width
+            }}
+         >
+            <KeyboardAvoidingView>
+               <View
+                  style={{
+                     marginTop: Dimensions.get('screen').height * 0.38,
+                     height: Dimensions.get('screen').height * 0.5,
+                     flex: 1,
+                     alignContent: 'center',
+                     padding: 50,
+                     width: '100%',
+                     backgroundColor: '#fff',
+                     borderTopStartRadius: 50,
+                     borderTopEndRadius: 50
+                  }}
+               >
+                  <Text
+                     style={{
+                        fontFamily: 'Roboto',
+                        fontSize: 23,
+                        marginBottom: 20
+                     }}
+                  >
+                     Login
+                  </Text>
+                  <TextInput
+                     placeholder="Email"
+                     placeholderTextColor="#000"
+                     textContentType="emailAddress"
+                     style={{
+                        padding: 2,
+                        borderBottomColor: '#000',
+                        borderBottomWidth: 0.8,
+                        width: '100%',
+                        fontSize: 14,
+                        marginTop: 10
+                     }}
+                  />
+                  <TextInput
+                     placeholder="Password"
+                     placeholderTextColor="#000"
+                     textContentType="password"
+                     style={{
+                        padding: 2,
+                        borderBottomColor: '#000',
+                        borderBottomWidth: 0.8,
+                        width: '100%',
+                        fontSize: 14,
+                        marginTop: 10
+                     }}
+                  />
+                  <TouchableOpacity
+                     style={{
+                        backgroundColor: '#3549FB',
+                        width: '100%',
+                        padding: 10,
+                        alignItems: 'center',
+                        borderRadius: 50,
+                        marginTop: 50
+                     }}
+                  >
+                     <Text style={{ color: '#fff' }}>Login</Text>
+                  </TouchableOpacity>
+               </View>
+            </KeyboardAvoidingView>
+         </ScrollView>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'orange'
+   },
+   gradient: {
+      flex: 1
+   }
+})
